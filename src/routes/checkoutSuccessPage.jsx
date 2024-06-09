ac6166ff720c57f4df032ec55c2072dcfa566eec
Musaab Abdalla <jamboosan209@gmail.com>
2024-06-09 14:02:09 +0200

    finishes styling checkout-success page

--- src/routes/checkoutSuccessPage.jsx
+++ src/routes/checkoutSuccessPage.jsx
@@ -1,9 +1,24 @@
+import { useNavigate} from "react-router-dom"
 
 
-
-function CheckoutSuccessPage(){
-  return(
-    <div>
+function CheckoutSuccessPage() {
+  const navigate = useNavigate()
+  return (
+    <div className="max-w-s mx-auto mt-20  ">
+      <div className="flex flex-col items-center gap-y-5">
+        <div class="flex items-center justify-center w-16 h-16 bg-green-500 text-white rounded-full">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+          </svg>
+        </div>
+        <div>
+          <p className="mb-3 text-gray-900 title-font font-bold text-xl">Thank you fort the order!</p>
+          <p className=""> Your order hand now been placed and you will shortly receive email confirmation.</p>
+        </div>
+            <button onClick={() => navigate("/")} type="button" className="mt-3 px-6 py-2 border rounded-md border-violet-600 bg-violet-600">
+              <span className="sr-only text-white sm:not-sr-only">Return to Shop</span>
+            </button>
+      </div>
     </div>
   )
 }
